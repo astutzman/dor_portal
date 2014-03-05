c10c0061a497865b022a7247bccf570c374f7a74
Andrew Stutzman <astutzman@coe.drexel.edu>
2014-03-05 16:09:57 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DorPortal::Application.config.secret_key_base = 'd769764e231682078e2dad4bd057578a82d03459d17734ab1743c01f366e7a22e355ffe12f6acbcfa9525a130ca60b831aa81be27e60837ae1be64b1641b9a5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DorPortal::Application.config.session_store :cookie_store, key: '_dor_portal_session'
